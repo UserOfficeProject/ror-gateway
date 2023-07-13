4385c89725dccc1569f827e1eee52caff7614061
Jekabs Karklins <enthusiast.studio@gmail.com>
2023-07-13 19:11:42 +0200

    chore(package.json): add cors package as a dependency to enable Cross-Origin Resource Sharing (CORS) support feat(index.ts): import and use cors middleware to enable CORS for all routes, allowing cross-origin requests

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import cors from "cors"; // Add this line
+import { configDotenv } from "dotenv";
 import express from "express";
 import health from "./queries/health";
 import organizations from "./queries/organizations";
-import { configDotenv } from "dotenv";
 
 configDotenv();
 
@@ -9,6 +10,9 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+
+// Enable CORS for all routes
+app.use(cors()); // Add this line
 
 app.get("/organizations", organizations);
 app.get("/health", health);
